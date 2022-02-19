3a0c683e98eb1cee37f1840b3ac0ea9b339baf55
frankandrobot <uriel@rstudio.com>
2022-02-19 14:07:22 -0600

    1 file modified

--- index.tsx
+++ index.tsx
@@ -8,12 +8,7 @@ import React, {
 import { render } from 'react-dom';
 import './style.css';
 
-interface AppProps {}
-interface AppState {
-  name: string;
-}
-
-function TestUseref() {
+function TestUseref1() {
   const valueRef = useRef(false);
   const [show, setShow] = useState(false);
 
@@ -26,18 +21,41 @@ function TestUseref() {
   const click = useCallback(() => (valueRef.current = true), []);
 
   return (
-    <>
+    <div>
       {show && <div>ref change triggered an effect</div>}
       <button onClick={click}>Click me</button>
-    </>
+    </div>
   );
+}
+
+function TestUseref2() {
+  const obj = useRef<{ value: boolean }>({ value: false });
+  const [done, setDone] = useState(false);
+
+  useEffect(function setObj() {
+    obj.current = { value: true };
+  }, []);
+
+  useEffect(
+    function triggerOnObj() {
+      if (obj.current.value) {
+        setDone(true);
+      }
+    },
+    [obj.current.value]
+  );
+
+  const anotherTrigger = done ? true : false;
+
+  return anotherTrigger ? <div>done</div> : null;
 }
 
 function App() {
   return (
-    <>
-      <TestUseref />
-    </>
+    <div>
+      <TestUseref1 />
+      <TestUseref2 />
+    </div>
   );
 }
 
